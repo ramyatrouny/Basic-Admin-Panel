2d4e8f7e7338c83fec681c3919b97a7caee6cf5a
Rami Atrouni <ramiatrouni@gmail.com>
2024-06-05 13:45:34 +0300

    Basic Posts Table

---
+++ components/posts/PostsTable.tsx
@@ -0,0 +1,39 @@
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+	TableCaption,
+} from '@/components/ui/table';
+import Link from 'next/link';
+import posts from '@/data/posts';
+import { Post } from '@/types/posts';
+
+interface PostsTableProps {
+	limit?: number;
+	title?: string;
+}
+
+const PostsTable = ({ limit, title }: PostsTableProps) => {
+	return (
+		<div className='mt-10'>
+			<h3 className='text-2xl mb-4 font-semibold'>{title ? title : 'Posts'}</h3>
+			<Table>
+				<TableCaption>A List of recent Posts</TableCaption>
+				<TableHeader>
+					<TableRow>
+						<TableHead>Title</TableHead>
+						<TableHead className='hidden md:table-cell'>Author</TableHead>
+						<TableHead className='hidden md:table-cell text-right'>
+							Date
+						</TableHead>
+					</TableRow>
+				</TableHeader>
+			</Table>
+		</div>
+	);
+};
+
+export default PostsTable;
