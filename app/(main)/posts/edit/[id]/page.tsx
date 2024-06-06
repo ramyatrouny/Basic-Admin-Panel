7b0004e025331d1dba29459a408dce9da6d99a05
Rami Atrouni <ramiatrouni@gmail.com>
2024-06-06 13:55:56 +0300

    Handle Edit Post + Toast

--- app/(main)/posts/edit/[id]/page.tsx
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -1,9 +1,156 @@
+'use client';
 import BackButton from '@/components/BackButton';
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { Button } from '@/components/ui/button';
+import posts from '@/data/posts';
+import { useToast } from '@/components/ui/use-toast';
 
-const PostsEditPage = () => {
+const formSchema = z.object({
+	title: z.string().min(3, {
+		message: 'Title must be at least 3 characters long',
+	}),
+	body: z.string().min(3, {
+		message: 'Body must be at least 3 characters long',
+	}),
+	author: z.string().min(3, {
+		message: 'Author must be at least 3 characters long',
+	}),
+	date: z.string().min(3, {
+		message: 'Date is required',
+	}),
+});
+
+interface PostEditPageProps {
+	params: {
+		id: string;
+	};
+}
+
+const PostsEditPage = ({ params }: PostEditPageProps) => {
+	const { toast } = useToast();
+	const post = posts.find((post) => post.id === params.id);
+
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			title: post?.title || '',
+			body: post?.body || '',
+			author: post?.author || '',
+			date: post?.date || '',
+		},
+	});
+
+	const handleSubmit = (data: z.infer<typeof formSchema>) => {
+		toast({
+			title: 'Post Has Been Updated',
+			description: `Post with title ${data.title} has been updated`,
+		});
+	};
+
 	return (
 		<>
 			<BackButton link='/posts' text='Back to posts' />
+			<h3 className='text-2xl mb-4'>Edit Post</h3>
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(handleSubmit)} className='space-y-8'>
+					<FormField
+						control={form.control}
+						name='title'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel className='uppercase text-xs font-bold text-zinc-500 dark:text-white'>
+									Title
+								</FormLabel>
+								<FormControl>
+									<Input
+										className='bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0'
+										placeholder='Enter Title'
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+
+					<FormField
+						control={form.control}
+						name='body'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel className='uppercase text-xs font-bold text-zinc-500 dark:text-white'>
+									Body
+								</FormLabel>
+								<FormControl>
+									<Textarea
+										className='bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0'
+										placeholder='Enter Body'
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+
+					<FormField
+						control={form.control}
+						name='author'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel className='uppercase text-xs font-bold text-zinc-500 dark:text-white'>
+									Author
+								</FormLabel>
+								<FormControl>
+									<Input
+										className='bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0'
+										placeholder='Enter Author'
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+
+					<FormField
+						control={form.control}
+						name='date'
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel className='uppercase text-xs font-bold text-zinc-500 dark:text-white'>
+									Date
+								</FormLabel>
+								<FormControl>
+									<Input
+										className='bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible:ring-offset-0'
+										placeholder='Enter Date'
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+
+					<Button className='w-full dark:bg-slate-800 dark:text-white'>
+						Update Post
+					</Button>
+				</form>
+			</Form>
 		</>
 	);
 };
