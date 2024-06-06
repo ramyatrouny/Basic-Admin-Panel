5e11e5b7877308639414b285ea5106a5e4a5d06d
Rami Atrouni <ramiatrouni@gmail.com>
2024-06-06 15:23:53 +0300

    enable system theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 				<ThemeProvider
 					attribute='class'
 					defaultTheme='light'
-					enableSystem={false}
+					enableSystem={true}
 					storageKey='dashboard-theme'
 				>
 					<Navbar />
