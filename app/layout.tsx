450bdca3f59e49fc72931f2c98b5d4e34544ee34
Rami Atrouni <ramiatrouni@gmail.com>
2024-06-06 14:46:37 +0300

    Dark and light theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import Navbar from '@/components/Navbar';
 import Sidebar from '@/components/Sidebar';
 import { Toaster } from '@/components/ui/toaster';
+import { ThemeProvider } from '@/components/providers/ThemeProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,14 +21,21 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<body className={inter.className}>
-				<Navbar />
-				<div className='flex'>
-					<div className='h-[100vh] md:block hidden w-[350px]'>
-						<Sidebar />
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='light'
+					enableSystem={false}
+					storageKey='dashboard-theme'
+				>
+					<Navbar />
+					<div className='flex'>
+						<div className='h-[100vh] md:block hidden w-[350px]'>
+							<Sidebar />
+						</div>
+						<div className='p-5 w-full md:max-w-[1140px]'>{children}</div>
 					</div>
-					<div className='p-5 w-full md:max-w-[1140px]'>{children}</div>
-				</div>
-				<Toaster />
+					<Toaster />
+				</ThemeProvider>
 			</body>
 		</html>
 	);
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,6 +10,7 @@ import {
 	DropdownMenuSeparator,
 	DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import ThemeToggler from './ThemeToggler';
 
 const Navbar = () => {
 	return (
@@ -18,24 +19,27 @@ const Navbar = () => {
 				<Image src={logo} alt='AtrouniMedia' width={80} />
 			</Link>
 
-			<DropdownMenu>
-				<DropdownMenuTrigger className='focus:outline-none'>
-					<Avatar>
-						<AvatarImage src='https://github.com/shadcn.png' alt='Avatar' />
-						<AvatarFallback className='text-black'>RA</AvatarFallback>
-					</Avatar>
-				</DropdownMenuTrigger>
-				<DropdownMenuContent>
-					<DropdownMenuLabel>My Account</DropdownMenuLabel>
-					<DropdownMenuSeparator />
-					<DropdownMenuItem>
-						<Link href={'/profile'}>Profile</Link>
-					</DropdownMenuItem>
-					<DropdownMenuItem>
-						<Link href={'/auth'}>Logout</Link>
-					</DropdownMenuItem>
-				</DropdownMenuContent>
-			</DropdownMenu>
+			<div className='flex items-center'>
+				<ThemeToggler />
+				<DropdownMenu>
+					<DropdownMenuTrigger className='focus:outline-none'>
+						<Avatar>
+							<AvatarImage src='https://github.com/shadcn.png' alt='Avatar' />
+							<AvatarFallback className='text-black'>RA</AvatarFallback>
+						</Avatar>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent>
+						<DropdownMenuLabel>My Account</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem>
+							<Link href={'/profile'}>Profile</Link>
+						</DropdownMenuItem>
+						<DropdownMenuItem>
+							<Link href={'/auth'}>Logout</Link>
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			</div>
 		</div>
 	);
 };
