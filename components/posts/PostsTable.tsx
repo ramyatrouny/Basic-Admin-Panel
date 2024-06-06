6737df724443cd8e6d9ef478aa988858eec56ae6
Rami Atrouni <ramiatrouni@gmail.com>
2024-06-06 09:44:06 +0300

    Handle table data

--- components/posts/PostsTable.tsx
+++ components/posts/PostsTable.tsx
@@ -29,8 +29,29 @@ const PostsTable = ({ limit, title }: PostsTableProps) => {
 						<TableHead className='hidden md:table-cell text-right'>
 							Date
 						</TableHead>
+						<TableHead>View</TableHead>
 					</TableRow>
 				</TableHeader>
+				<TableBody>
+					{posts.map((post) => (
+						<TableRow key={post.id}>
+							<TableCell>{post.title}</TableCell>
+							<TableCell className='hidden md:table-cell'>
+								{post.author}
+							</TableCell>
+							<TableCell className='text-right hidden md:table-cell'>
+								{post.date}
+							</TableCell>
+							<TableCell>
+								<Link href={`/posts/edit/${post.id}`}>
+									<button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-xs'>
+										Edit
+									</button>
+								</Link>
+							</TableCell>
+						</TableRow>
+					))}
+				</TableBody>
 			</Table>
 		</div>
 	);
