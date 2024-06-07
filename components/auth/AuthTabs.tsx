098bd3dd58e00c64effbabca24dc4ac37f95f75f
Rami Atrouni <ramiatrouni@gmail.com>
2024-06-07 12:48:56 +0300

    add authentication

--- components/auth/AuthTabs.tsx
+++ components/auth/AuthTabs.tsx
@@ -1,4 +1,6 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import LoginForm from './LoginForm';
+import RegisterForm from './RegisterForm';
 
 const AuthTabs = () => {
 	return (
@@ -7,8 +9,12 @@ const AuthTabs = () => {
 				<TabsTrigger value='login'>Login</TabsTrigger>
 				<TabsTrigger value='register'>Register</TabsTrigger>
 			</TabsList>
-			<TabsContent value='login'>login</TabsContent>
-			<TabsContent value='register'>register</TabsContent>
+			<TabsContent value='login'>
+				<LoginForm />
+			</TabsContent>
+			<TabsContent value='register'>
+				<RegisterForm />
+			</TabsContent>
 		</Tabs>
 	);
 };
